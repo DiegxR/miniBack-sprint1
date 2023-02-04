852fe53c288f3f1b64999ea2e7660f65735b1975
DiegxR <juandiegorojasarredono@gmail.com>
2023-02-03 21:04:27 -0500

    try 2 solution cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 3000;
 
 server.use((req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next()
 })
 server.use(router)
